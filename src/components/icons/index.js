5c65deefe6d72619b1be544b65e3b0762fc04b16
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-05-07 17:34:43 +0300

    update

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,5 +1,5 @@
 import { GitHubIcon } from "./GitHubIcon";
-import { LinkedInIcon } from "./LinkedinIcon"; // Renamed the import statement to match the file name
+import { LinkedInIcon } from "./LinkedInIcon";
 import { TwitterIcon } from "./TwitterIcon";
 
 export { GitHubIcon, LinkedInIcon, TwitterIcon };
