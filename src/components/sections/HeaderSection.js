ba44578efa7fa3c1075afc45a35eeb99a2dfb443
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2025-02-01 23:33:39 +0300

    refactor: Simplify HeaderSection component structure and improve readability

--- src/components/sections/HeaderSection.js
+++ src/components/sections/HeaderSection.js
@@ -4,69 +4,69 @@ import ThemeSwitch from "@/components/ThemeSwitch";
 import { GlobeIcon, MailIcon, PhoneIcon } from "lucide-react";
 import Link from "next/link";
 
-const HeaderSection = ({ resumeData }) => (
-  <div className="flex flex-col-reverse items-start justify-between gap-6 sm:gap-8 md:flex-row md:items-center">
-    <div className="flex-1 space-y-4">
-      <div className="flex items-center justify-between gap-4">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold text-gray-900 dark:text-white font-lora">
-          {resumeData.name}
-        </h1>
-        <ThemeSwitch />
-      </div>
-      <p className="text-sm sm:text-base md:text-lg text-pretty font-mono text-gray-600 dark:text-[#B8B8B8]">
-        {resumeData.about}
-      </p>
-      <div className="flex items-center gap-2 font-mono text-sm sm:text-base text-gray-600 dark:text-[#B8B8B8]">
-        <GlobeIcon className="h-4 w-4" />
-        <Link
-          className="hover:text-blue-500 hover:underline"
-          href={resumeData.locationLink}
-          target="_blank"
-        >
-          {resumeData.location}
-        </Link>
-      </div>
-      <div className="flex gap-2 print:hidden">
-        <Button
-          className="group h-9 w-9 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
-          variant="outline"
-          size="icon"
-          asChild
-        >
-          <Link href={`mailto:${resumeData.contact.email}`}>
-            <MailIcon className="h-4 w-4 text-gray-600 transition-colors group-hover:text-blue-500 dark:text-gray-400" />
+export default function HeaderSection({ resumeData }){
+  return (
+    <div className="flex flex-col-reverse items-start justify-between gap-6 sm:gap-8 md:flex-row md:items-center">
+      <div className="flex-1 space-y-4">
+        <div className="flex items-center justify-between gap-4">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold text-gray-900 dark:text-white font-lora">
+            {resumeData.name}
+          </h1>
+          <ThemeSwitch />
+        </div>
+        <p className="text-sm sm:text-base md:text-lg text-pretty font-mono text-gray-600 dark:text-[#B8B8B8]">
+          {resumeData.about}
+        </p>
+        <div className="flex items-center gap-2 font-mono text-sm sm:text-base text-gray-600 dark:text-[#B8B8B8]">
+          <GlobeIcon className="h-4 w-4" />
+          <Link
+            className="hover:text-blue-500 hover:underline"
+            href={resumeData.locationLink}
+            target="_blank"
+          >
+            {resumeData.location}
           </Link>
-        </Button>
-        <Button
-          className="group h-9 w-9 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
-          variant="outline"
-          size="icon"
-          asChild
-        >
-          <Link href={`tel:${resumeData.contact.tel}`}>
-            <PhoneIcon className="h-4 w-4 text-gray-600 transition-colors group-hover:text-blue-500 dark:text-gray-400" />
-          </Link>
-        </Button>
-        {resumeData.contact.social.map((social) => (
+        </div>
+        <div className="flex gap-2 print:hidden">
           <Button
-            key={social.name}
             className="group h-9 w-9 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
             variant="outline"
             size="icon"
             asChild
           >
-            <Link href={social.url} target="_blank">
-              <social.icon className="h-4 w-4 text-gray-600 transition-colors group-hover:text-blue-500 dark:text-gray-400" />
+            <Link href={`mailto:${resumeData.contact.email}`}>
+              <MailIcon className="h-4 w-4 text-gray-600 transition-colors group-hover:text-blue-500 dark:text-gray-400" />
             </Link>
           </Button>
-        ))}
+          <Button
+            className="group h-9 w-9 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
+            variant="outline"
+            size="icon"
+            asChild
+          >
+            <Link href={`tel:${resumeData.contact.tel}`}>
+              <PhoneIcon className="h-4 w-4 text-gray-600 transition-colors group-hover:text-blue-500 dark:text-gray-400" />
+            </Link>
+          </Button>
+          {resumeData.contact.social.map((social) => (
+            <Button
+              key={social.name}
+              className="group h-9 w-9 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700"
+              variant="outline"
+              size="icon"
+              asChild
+            >
+              <Link href={social.url} target="_blank">
+                <social.icon className="h-4 w-4 text-gray-600 transition-colors group-hover:text-blue-500 dark:text-gray-400" />
+              </Link>
+            </Button>
+          ))}
+        </div>
       </div>
+      <Avatar className="h-24 w-24 sm:h-32 sm:w-32 rounded-xl ring-2 ring-gray-200 dark:ring-gray-800">
+        <AvatarImage alt={resumeData.name} src={resumeData.avatarUrl} />
+        <AvatarFallback>{resumeData.initials}</AvatarFallback>
+      </Avatar>
     </div>
-    <Avatar className="h-24 w-24 sm:h-32 sm:w-32 rounded-xl ring-2 ring-gray-200 dark:ring-gray-800">
-      <AvatarImage alt={resumeData.name} src={resumeData.avatarUrl} />
-      <AvatarFallback>{resumeData.initials}</AvatarFallback>
-    </Avatar>
-  </div>
-);
-
-export default HeaderSection;
+  )
+};
--- src/components/sections/MainContent.js
+++ src/components/sections/MainContent.js
@@ -5,148 +5,166 @@ import { ProjectCard } from "@/components/project-card";
 import Link from "next/link";
 import { ExternalLink } from "lucide-react";
 
-const MainContent = ({ resumeData }) => (
-  <div className="mt-6 sm:mt-8 grid gap-6 sm:gap-8">
-    {/* About Section */}
-    <Section>
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
-        About
-      </h2>
-      <p className="text-sm sm:text-base md:text-lg text-pretty font-mono text-gray-600 dark:text-[#B8B8B8]">
-        {resumeData.summary}
-      </p>
-    </Section>
+export default function ({ resumeData }) {
+  const githubUrl = resumeData.contact.social.find(
+    (social) => social.name === "GitHub"
+  )?.url;
 
-    {/* Work Experience Section */}
-    <Section>
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
-        Work Experience
-      </h2>
-      <ul className="list-disc list-outside pl-5 space-y-4 text-gray-900 dark:text-white">
-        {resumeData.work.map((work) => (
-          <li key={work.company}>
-            <Card className="group overflow-hidden">
-              <CardHeader className="flex flex-col gap-2">
-                <div className="flex items-center justify-between gap-4">
-                  <h2 className="inline-flex items-center gap-2 font-bold text-gray-900 dark:text-white">
-                    <Link
-                      href={work.link}
-                      target="_blank"
-                      className="text-base sm:text-lg font-semibold text-gray-900 hover:text-blue-500 dark:text-white dark:hover:text-blue-400 flex items-center gap-1 group"
-                    >
-                      {work.company}
-                      <ExternalLink className="h-4 w-4 text-gray-500 dark:text-gray-400 group-hover:text-blue-500 dark:group-hover:text-blue-400" />
-                    </Link>
-                    <div className="flex gap-1">
-                      {work.badges.map((badge) => (
-                        <Badge
-                          variant="secondary"
-                          className="bg-blue-100 text-xs text-blue-700 dark:bg-blue-900 dark:text-blue-100"
-                          key={badge}
-                        >
-                          {badge}
-                        </Badge>
-                      ))}
+  return (
+    <div className="mt-6 sm:mt-8 grid gap-6 sm:gap-8">
+      {/* About Section */}
+      <Section>
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
+          About
+        </h2>
+        <p className="text-sm sm:text-base md:text-lg text-pretty font-mono text-gray-600 dark:text-[#B8B8B8]">
+          {resumeData.summary}
+        </p>
+      </Section>
+
+      {/* Work Experience Section */}
+      <Section>
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
+          Work Experience
+        </h2>
+        <ul className="list-disc list-outside pl-5 space-y-4 text-gray-900 dark:text-white">
+          {resumeData.work.map((work) => (
+            <li key={work.company}>
+              <Card className="group overflow-hidden">
+                <CardHeader className="flex flex-col gap-2">
+                  <div className="flex items-center justify-between gap-4">
+                    <h2 className="inline-flex items-center gap-2 font-bold text-gray-900 dark:text-white">
+                      <Link
+                        href={work.link}
+                        target="_blank"
+                        className="text-base sm:text-lg font-semibold text-gray-900 hover:text-blue-500 dark:text-white dark:hover:text-blue-400 flex items-center gap-1 group"
+                      >
+                        {work.company}
+                        <ExternalLink className="h-4 w-4 text-gray-500 dark:text-gray-400 group-hover:text-blue-500 dark:group-hover:text-blue-400" />
+                      </Link>
+                      <div className="flex gap-1">
+                        {work.badges.map((badge) => (
+                          <Badge
+                            variant="secondary"
+                            className="bg-blue-100 text-xs text-blue-700 dark:bg-blue-900 dark:text-blue-100"
+                            key={badge}
+                          >
+                            {badge}
+                          </Badge>
+                        ))}
+                      </div>
+                    </h2>
+                    <div className="text-base sm:text-lg text-gray-900 dark:text-white">
+                      {work.start} - {work.end}
                     </div>
+                  </div>
+                </CardHeader>
+                <CardContent className="text-sm sm:text-base text-pretty font-mono text-gray-600 dark:text-[#B8B8B8] mt-2">
+                  {work.description}
+                </CardContent>
+              </Card>
+            </li>
+          ))}
+        </ul>
+      </Section>
+
+      {/* Education Section */}
+      <Section>
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
+          Education
+        </h2>
+        <div className="grid gap-4">
+          {resumeData.education.map((education) => (
+            <Card key={education.school}>
+              <CardHeader>
+                <div className="flex flex-col gap-1">
+                  <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    {education.school}
                   </h2>
                   <div className="text-base sm:text-lg text-gray-900 dark:text-white">
-                    {work.start} - {work.end}
+                    {education.start} - {education.end}
                   </div>
                 </div>
               </CardHeader>
-              <CardContent className="text-sm sm:text-base text-pretty font-mono text-gray-600 dark:text-[#B8B8B8] mt-2">
-                {work.description}
+              <CardContent className="text-sm sm:text-base text-pretty font-mono text-gray-600 dark:text-[#B8B8B8]">
+                {education.degree}
               </CardContent>
             </Card>
-          </li>
-        ))}
-      </ul>
-    </Section>
+          ))}
+        </div>
+      </Section>
 
-    {/* Education Section */}
-    <Section>
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
-        Education
-      </h2>
-      <div className="grid gap-4">
-        {resumeData.education.map((education) => (
-          <Card key={education.school}>
-            <CardHeader>
-              <div className="flex flex-col gap-1">
-                <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
-                  {education.school}
-                </h2>
-                <div className="text-base sm:text-lg text-gray-900 dark:text-white">
-                  {education.start} - {education.end}
+      {/* Languages Section */}
+      <Section>
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
+          Languages
+        </h2>
+        <div className="grid gap-4">
+          {resumeData.languages.map((lang) => (
+            <Card key={lang.name}>
+              <CardHeader>
+                <div className="flex items-center justify-between">
+                  <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
+                    {lang.name}
+                  </h2>
+                  <Badge className="bg-blue-100 text-xs text-blue-700 dark:bg-blue-900 dark:text-blue-100">
+                    {lang.level}
+                  </Badge>
                 </div>
-              </div>
-            </CardHeader>
-            <CardContent className="text-sm sm:text-base text-pretty font-mono text-gray-600 dark:text-[#B8B8B8]">
-              {education.degree}
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-    </Section>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
+      </Section>
 
-    {/* Languages Section */}
-    <Section>
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
-        Languages
-      </h2>
-      <div className="grid gap-4">
-        {resumeData.languages.map((lang) => (
-          <Card key={lang.name}>
-            <CardHeader>
-              <div className="flex items-center justify-between">
-                <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
-                  {lang.name}
-                </h2>
-                <Badge className="bg-blue-100 text-xs text-blue-700 dark:bg-blue-900 dark:text-blue-100">
-                  {lang.level}
-                </Badge>
-              </div>
-            </CardHeader>
-          </Card>
-        ))}
-      </div>
-    </Section>
+      {/* Skills Section */}
+      <Section>
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
+          Skills
+        </h2>
+        <div className="flex flex-wrap gap-2">
+          {resumeData.skills.map((skill) => (
+            <Badge
+              key={skill}
+              className="bg-gray-100 text-sm text-gray-700 dark:bg-gray-800 dark:text-gray-300"
+            >
+              {skill}
+            </Badge>
+          ))}
+        </div>
+      </Section>
 
-    {/* Skills Section */}
-    <Section>
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
-        Skills
-      </h2>
-      <div className="flex flex-wrap gap-2">
-        {resumeData.skills.map((skill) => (
-          <Badge
-            key={skill}
-            className="bg-gray-100 text-sm text-gray-700 dark:bg-gray-800 dark:text-gray-300"
-          >
-            {skill}
-          </Badge>
-        ))}
-      </div>
-    </Section>
+      {/* Projects Section */}
+      <Section className="scroll-mb-16">
+        <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
+          Projects
+        </h2>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {resumeData.projects.map((project) => (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              tags={project.techStack}
+              link={"link" in project ? project.link.href : undefined}
+            />
+          ))}
+        </div>
 
-    {/* Projects Section */}
-    <Section className="scroll-mb-16">
-      <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900 dark:text-white">
-        Projects
-      </h2>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {resumeData.projects.map((project) => (
-          <ProjectCard
-            key={project.title}
-            title={project.title}
-            description={project.description}
-            tags={project.techStack}
-            link={"link" in project ? project.link.href : undefined}
-          />
-        ))}
-      </div>
-    </Section>
-  </div>
-);
-
-export default MainContent;
+        {/* "View More" Button */}
+        {githubUrl && (
+          <div className="mt-6 flex justify-center">
+            <Link
+              href={githubUrl}
+              target="_blank"
+              className="inline-flex items-center gap-2 text-sm sm:text-base font-medium text-blue-600 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300"
+            >
+              View More Projects
+              <ExternalLink className="h-4 w-4" />
+            </Link>
+          </div>
+        )}
+      </Section>
+    </div>
+  )
+};
